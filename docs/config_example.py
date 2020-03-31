09a322ec0c207bcae390a868e51e95277790c831
Logan Richardson <reviewedge56@gmail.com>
2020-03-30 23:37:27 -0400

    Create config_example.py

---
+++ docs/config_example.py
@@ -0,0 +1,33 @@
+# When you create your own configuration file, make sure it is named "config.py"
+
+# run track info:
+run_log_google_sheet_id = "XXXXX"
+
+
+# wiki of the day info:
+test_email_address = "XXXXX"
+test_email_password = "XXXXX"
+
+email_send_list_google_sheet_id = "XXXXX"
+
+send_wiki_email_address = "XXXXX"
+send_wiki_email_password = "XXXXX"
+
+
+# send_news info:
+sender_email_address = "XXXXX"
+sender_email_password = "XXXXX"
+rec_email_address = "XXXXX"
+
+
+# email info:
+reader_email_address = "XXXXX"
+reader_email_password = "XXXXX"
+
+
+# spotify info:
+spotify_username = "XXXXX"
+spotify_dev_id = "XXXXX"
+spotify_dev_secret = "XXXXX"
+song_log_sheet_id = "XXXXX"
+blacklist_sheet_id = "XXXXX"
