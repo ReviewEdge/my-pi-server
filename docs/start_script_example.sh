7683a6853f13a701ba8f91d78a4cab39f66e4b76
Logan Richardson <reviewedge56@gmail.com>
2020-04-16 17:03:00 -0400

    Update start_script_example.sh

--- docs/start_script_example.sh
+++ docs/start_script_example.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+
 # The customized version should be moved to the "sample" directory.
 
-#!/bin/bash
 echo "Running FILE_NAME.py..."
 cd /home/pi/MyPiServer/server/LOCATION_OF_FILE
 python3 FILE_NAME.py &
