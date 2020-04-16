3d105f235e285a8956cb32f500fefcab0d29dc94
Logan Richardson <reviewedge56@gmail.com>
2020-04-16 16:40:38 -0400

    Update config_example.py

--- docs/config_example.py
+++ docs/config_example.py
@@ -1,4 +1,4 @@
-# When you create your own configuration file, make sure it is named "config.py"
+# When you create your own configuration file, make sure it is named "config.py" and is moved into the server directory.
 
 # run track info:
 run_log_google_sheet_id = "XXXXX"
